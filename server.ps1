8183bd1a417c2695a5f2ca04f20a2c97ef968a67
danir1252008-bit <danir1252008-bit@users.noreply.github.com>
2026-08-07 14:46:26 +0530

    Add sitemap.xml and robots.txt for Google Search Console

--- server.ps1
+++ server.ps1
@@ -49,6 +49,8 @@ try {
                 ".html" { "text/html; charset=utf-8" }
                 ".css"  { "text/css; charset=utf-8" }
                 ".js"   { "application/javascript; charset=utf-8" }
+                ".xml"  { "application/xml; charset=utf-8" }
+                ".txt"  { "text/plain; charset=utf-8" }
                 ".png"  { "image/png" }
                 ".jpg"  { "image/jpeg" }
                 ".jpeg" { "image/jpeg" }
